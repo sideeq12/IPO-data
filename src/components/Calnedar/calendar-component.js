b4cd945f115363817ca2f13f3a9a615ed2a2936a
sideeq12 <abdwaheed2018@gmail.com>
2020-12-05 05:07:05 -0800

    calendar

---
+++ src/components/Calnedar/calendar-component.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import {Component} from "react";
+
+class Calendar extends Component{
+    constructor(){
+        super();
+        this.state ={
+            metricTable : "table"
+        }
+    }
+ 
+    render(){
+        return(<h2>HI there </h2>)
+    }
+}
