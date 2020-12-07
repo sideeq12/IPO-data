2f0b890428c15164bc20853b144443e4af3af310
sideeq12 <abdwaheed2018@gmail.com>
2020-12-07 07:05:24 -0800

    fetch

--- src/components/News-section/news-component.js
+++ src/components/News-section/news-component.js
@@ -10,8 +10,23 @@ class News extends Component{
     }
     componentDidMount(){
         let url = "https://api.stlouisfed.org/fred/series?series_id=SMU16142609092161101&api_key=c8c81bfff01e8985bbbb8e22555c04a9&file_type=json#"
-        fetch(url)
-        .then(response => response.json())
+
+        async function PostData(url = "", data = {}){
+            const response = await fetch(url, {
+                // method : "GET",
+                mode : "no-cors",
+                // headers : {
+                //     "Content-Type" : "application/json"
+                // },
+                body : JSON.stringify(data)
+            })
+            return response.json();
+        }
+        PostData(url, {check : 3})
+        .then(data => console.log(data))
+        // fetch(url, {mode : "no cors"})
+        // .then(response => response.json())
+        // .then(data => console.log(data))
     }
     render(){
         let news = this.state.News;
