610f6e66afb6cb0ef367c9c20cb8de4b9f6ac7c1
sideeq12 <abdwaheed2018@gmail.com>
2020-12-07 04:26:32 -0800

    displace

--- src/components/Monthlydetails/monthly-analysis.js
+++ src/components/Monthlydetails/monthly-analysis.js
@@ -8,13 +8,7 @@ class MonthlyDetails extends Component{
             MonthlyData : "MonthlyData"
         }
     }
-    componentDidMount(){
-        let url = "https://api.stlouisfed.org/fred/series?series_id=SMU16142609092161101&api_key=c8c81bfff01e8985bbbb8e22555c04a9&file_type=json#"
-        fetch(url)
-        .then(response => response.json())
-        
-      
-    }
+    
     render(){
         return(
             <div className="col-md-4">
--- src/components/News-section/news-component.js
+++ src/components/News-section/news-component.js
@@ -9,9 +9,9 @@ class News extends Component{
         }
     }
     componentDidMount(){
-        fetch()
-        .then()
-        .then()
+        let url = "https://api.stlouisfed.org/fred/series?series_id=SMU16142609092161101&api_key=c8c81bfff01e8985bbbb8e22555c04a9&file_type=json#"
+        fetch(url)
+        .then(response => response.json())
     }
     render(){
         let news = this.state.News;
